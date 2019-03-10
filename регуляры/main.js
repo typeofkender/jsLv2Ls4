6728acf3c00f550c4b8b6cab571e573981965401
typeofkender <san_9@inbox.ru>
2019-03-11 00:11:30 +0400

    дз к 4 уроку

---
+++ регуляры/main.js
@@ -0,0 +1,19 @@
+//1 quest
+const text = 'One: \'Hi Mary.\'\n' +
+    'Two: \'Oh, hi.\'\n' +
+    'One: \'How are you doing?\'\n' +
+    'Two: \'I\'m doing alright. How about you?\'\n' +
+    'One: \'Not too bad. The weather is great isn\'t it?\'\n' +
+    'Two: \'Yes. It\'s absolutely beautiful today.\'\n' +
+    'One: \'I wish it was like this more frequently.\'\n' +
+    'Two: \'Me too.\'\n' +
+    'One: \'So where are you going now?\'\n' +
+    'Two: \'I\'m going to meet a friend of mine at the department store.\'\n' +
+    'One: \'Going to do a little shopping?\'\n' +
+    'Two: \'Yeah, I have to buy some presents for my parents.\'\n' +
+    'One: \'What\'s the occasion?\'\n' +
+    'Two: \'It\'s their anniversary.\'\n' +
+    'One: \'That\'s great. Well, you better get going. You don\'t want to be late.\'\n' +
+    'Two: \'I\'ll see you next time.\'\n' +
+    'One: \'Sure. Bye.\'';
+console.log(text.replace(/'$/gm, '"').replace(/\s'/gm, ' "'));
